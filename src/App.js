863cf9605ad0873a1405a8c7d28ea138bda73536
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-09-01 11:08:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([
+		{ itemName: 'item 1', quantity: 1, isSelected: false },
+		{ itemName: 'item 2', quantity: 3, isSelected: true },
+		{ itemName: 'item 3', quantity: 2, isSelected: false },
+	]);
+
+	const [inputValue, setInputValue] = useState('');
+	const [totalItemCount, setTotalItemCount] = useState(6);
+
+	const handleAddButtonClick = () => {
+		const newItem = {
+			itemName: inputValue,
+			quantity: 1,
+			isSelected: false,
+		};
+
+		const newItems = [...items, newItem];
+
+		setItems(newItems);
+		setInputValue('');
+		calculateTotal();
+	};
+
+	const handleQuantityIncrease = (index) => {
+		const newItems = [...items];
+
+		newItems[index].quantity++;
+
+		setItems(newItems);
+		calculateTotal();
+	};
+
+	const handleQuantityDecrease = (index) => {
+		const newItems = [...items];
+
+		newItems[index].quantity--;
+
+		setItems(newItems);
+		calculateTotal();
+	};
+
+	const toggleComplete = (index) => {
+		const newItems = [...items];
+
+		newItems[index].isSelected = !newItems[index].isSelected;
+
+		setItems(newItems);
+	};
+
+	const calculateTotal = () => {
+		const totalItemCount = items.reduce((total, item) => {
+			return total + item.quantity;
+		}, 0);
+
+		setTotalItemCount(totalItemCount);
+	};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className='app-background'>
+			<div className='main-container'>
+				<div className='add-item-box'>
+					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
+				</div>
+				<div className='item-list'>
+					{items.map((item, index) => (
+						<div className='item-container'>
+							<div className='item-name' onClick={() => toggleComplete(index)}>
+								{item.isSelected ? (
+									<>
+										<FontAwesomeIcon icon={faCheckCircle} />
+										<span className='completed'>{item.itemName}</span>
+									</>
+								) : (
+									<>
+										<FontAwesomeIcon icon={faCircle} />
+										<span>{item.itemName}</span>
+									</>
+								)}
+							</div>
+							<div className='quantity'>
+								<button>
+									<FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityDecrease(index)} />
+								</button>
+								<span> {item.quantity} </span>
+								<button>
+									<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+				<div className='total'>Total: {totalItemCount}</div>
+			</div>
+		</div>
+	);
+
+
+
+
+  // return (
+  //   <div className="App">
+  //     {/* <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload. blablabalblabal
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header> */}
+  //   </div>
+  // );
 }
 
 export default App;
