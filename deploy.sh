465d5b57292178c99048746c1154b542b24af76b
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-09-29 12:03:13 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo "This is the run command"
 docker container rm shoppinglist-container || true
 docker image rm 10.0.40.160:5000/shoppinglist/test:0.0.1 || true
 docker pull 10.0.40.160:5000/shoppinglist/test:0.0.1  
-docker run -d --name shoppinglist-container 10.0.40.160:5000/shoppinglist/test:0.0.1
+docker run --platform linux/amd64 -d --name shoppinglist-container 10.0.40.160:5000/shoppinglist/test:0.0.1
