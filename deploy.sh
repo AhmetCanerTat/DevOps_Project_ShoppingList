a8728fa497cbbc47684e0b1256cd8da3d9ac058a
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-09-29 12:39:17 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo "This is the run command"
 docker container rm shoppinglist-container || true
 docker image rm 10.0.40.160:5000/shoppinglist/test:0.0.3|| true
 docker pull 10.0.40.160:5000/shoppinglist/test:0.0.3  
-docker run --platform linux/amd64 -d --name shoppinglist-container 10.0.40.160:5000/shoppinglist/test:0.0.3
+docker run --platform linux/amd64 -d -p 8081:80 --name shoppinglist-container 10.0.40.160:5000/shoppinglist/test:0.0.3
--- run.Dockerfile
+++ run.Dockerfile
@@ -2,7 +2,7 @@ FROM --platform=linux/amd64 nginx:mainline-alpine3.18
 
 COPY build /usr/share/nginx/html
 
-WORKDIR /usr/share/nginx/html
+
 
 
 
