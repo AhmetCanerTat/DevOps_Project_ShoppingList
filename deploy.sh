43bad51d38419b2ab0f885320af8ffd201946979
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-09-29 11:55:45 +0200

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "This is the run command"
 docker container rm shoppinglist-container || true
-docker image rm test:0.0.1 || true
+docker image rm 10.0.40.160:5000/shoppinglist/test:0.0.1 || true
 docker pull 10.0.40.160:5000/shoppinglist/test:0.0.1  
-docker run --name shoppinglist-container test:0.0.1 
+docker run -d --name shoppinglist-container 10.0.40.160:5000/shoppinglist/test:0.0.1
