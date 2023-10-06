06e6074c0b240f9b6a7d2d364521c796bba24b20
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-10-06 12:41:15 +0200

    l

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ docker container stop shoppinglist-container-test || true
 docker container rm shoppinglist-container-test || true
 docker image rm 10.0.40.160:5000/shoppinglist/test:0.0.3|| true
 docker pull 10.0.40.160:5000/shoppinglist/test:0.0.3  
-docker run --platform linux/amd64 -d -p 8081:80 --name shoppinglist-container-test 10.0.40.160:5000/shoppinglist/test:0.0.3
+docker run --platform linux/amd64 -d -p 8082:80 --name shoppinglist-container-test 10.0.40.160:5000/shoppinglist/test:0.0.3
