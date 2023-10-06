52341f872f4cf81c6de383bef14b12b60b608efc
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-10-06 11:27:02 +0200

    added tests

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 npm install
 npm i --save @fortawesome/fontawesome-svg-core
+npm test
 npm run build
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+test("test something",()=>{
+
+  render(<App/>);
+  const testElement = screen.getByPlaceholderText(/Add an item.../)
+  expect(testElement).toBeInTheDocument();
+
+
+})
