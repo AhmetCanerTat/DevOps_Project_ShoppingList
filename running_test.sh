ced584dc3c90af9000bb8176e9df17cede13f4cd
iven germonpre <iven.germonpre@gmail.com>
2023-10-06 12:47:40 +0200

    added a test for testing if the server is online

---
+++ running_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+response=$(curl -Is 10.0.40.162:8081 | head -n 1)
+
+if [[ "$response" == "HTTP/1.1 200 OK"* ]]; then
+    echo "Server responded with 200 OK."
+    # Add any additional commands you want to run on success here
+else
+    echo "Server did not respond with 200 OK. Exiting."
+    exit 5
+fi
