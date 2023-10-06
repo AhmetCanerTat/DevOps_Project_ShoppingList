e7fda6fa4d3529c489cb2a0c66a5334d679b43a6
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-10-06 12:16:41 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "This is the run command"
-docker container stop shoppinglist-container || true
-docker container rm shoppinglist-container || true
+docker container stop shoppinglist-container-$1 || true
+docker container rm shoppinglist-container-$1 || true
 docker image rm 10.0.40.160:5000/shoppinglist/test:0.0.3|| true
 docker pull 10.0.40.160:5000/shoppinglist/test:0.0.3  
-docker run --platform linux/amd64 -d -p 8081:80 --name shoppinglist-container 10.0.40.160:5000/shoppinglist/test:0.0.3
+docker run --platform linux/amd64 -d -p 8081:80 --name shoppinglist-container-$1 10.0.40.160:5000/shoppinglist/test:0.0.3
